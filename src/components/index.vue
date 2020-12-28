fbe8fdbc9e860b9baa816df3d4c2f9cb51178b91
Dieinwarm <coolseamonster@qq.com>
2020-12-28 15:51:49 +0800

    搞定路由和数字变动的动画

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,34 +1,39 @@
 <template>
 	<div>
 		<div class="header">🐶 舔狗的艺术</div>
-		
+		<el-row type="flex" justify="center">
+			<el-col :xs="22" :sm="18" :lg="12">
+				<el-header height="61px">
+					<el-menu mode = "horizontal" text-color = "#2C3E50" active-text-color = "#3498DB" border-bottom = "none" router :default-active = "path">
+						<el-menu-item  v-for="(item, index) in nav_menu_data" :index = "item.path" :key = "index">
+							{{item.title}}
+						</el-menu-item>
+					</el-menu>
+				</el-header>
+				<router-view></router-view>
+			</el-col>
+		</el-row>
 	</div>
 </template>
 
 <script>
-	export default{
-		data() {
-			return {
-                path: [],
-                nav_menu_data: [{
-					title: '舔狗の语',
-					path: '/words'
-				}, {
-					title: '舔狗日记',
-					path: '/diary'
-				}],
-			}
-		},
-		methods:{
-			onRouteChanged () {
-				this.path  = [this.$route.path];
-			}
-		},
-		created () {
-			this.onRouteChanged()
-		},
-		watch: {
-			'$route': 'onRouteChanged'
+import { ref, reactive } from 'vue';
+export default{
+	setup(){
+		const path = ref("/words");
+		const nav_menu_data = reactive(
+			[{
+				title: '舔狗の语',
+				path: '/words'
+			}, {
+				title: '舔狗日记',
+				path: '/diary'
+			}]
+		);
+		return {
+			path,
+			nav_menu_data
 		}
 	}
+}
 </script>
--- src/components/view/words.vue
+++ src/components/view/words.vue
@@ -1,5 +1,52 @@
 <template>
     <div>
-        
+        <div class="content-box">
+            <el-card class="box-card">
+                <div class="content" v-cloak>
+                    <div class="text">{{ content }}</div>
+                    <div class="lickCount">👅x {{ animatedNumber }}</div>
+                </div>
+            </el-card>
+        </div>
+        <div class="buttons">
+            <el-button type="primary" @click="getWords" round>再来一条</el-button>
+            <el-button @click="lick" round :disabled="clickLick">👅</el-button>
+        </div>
     </div>
 </template>
+
+<script>
+import { computed, ref, watch } from 'vue';
+import { gsap } from 'gsap';
+
+export default {
+    setup(){
+        const lickCount = ref(0);
+        const tweenedNumber = ref(0);
+        const clickLick = ref(false);
+        const content = ref("asdasda打算大大大大人");
+        const getWords = () => {
+
+        };
+        const lick = () => {
+            clickLick.value = true;
+            lickCount.value += 20 ;
+        }
+        const animatedNumber = computed(() => {
+            return tweenedNumber.value.toFixed(0);
+        });
+        watch(lickCount,(nv) => {
+            tweenedNumber.value = 0;
+            gsap.to(tweenedNumber, { duration: 0.5, value: nv });
+        });
+        return {
+            lickCount,
+            clickLick,
+            content,
+            getWords,
+            lick,
+            animatedNumber
+        }
+    }
+}
+</script>
