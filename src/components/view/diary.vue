88760b4560de5608399df9dc33edc220709ce64a
Dieinwarm <coolseamonster@qq.com>
2021-01-04 18:21:00 +0800

    完善

--- src/components/view/diary.vue
+++ src/components/view/diary.vue
@@ -1,5 +1,95 @@
 <template>
-    <div>
-        
-    </div>
+	<div>
+		<div class="diary-content-box">
+			<el-card class="box-card">
+				<div class="diary-content" v-cloak>
+					<div class="diary-text">
+						<p>{{ citystr }} <br> {{ weatherstr }}</p>
+						<p>{{ content }}</p>
+					</div>
+				</div>
+			</el-card>
+		</div>
+		<div class="buttons">
+			<el-button type="primary" @click="getDiary" round>再来一份</el-button>
+			<el-button type="success" @click="addDiary" round>投稿</el-button>
+		</div>
+	</div>
 </template>
+
+<script>
+import { onMounted, ref, getCurrentInstance } from 'vue'
+
+export default {
+	setup(){
+		const ctx = getCurrentInstance();
+		const citystr = ref("");
+		const content = ref("");
+		const weatherstr = ref("");
+
+		const getweather = () => {
+			let date = new Date();
+			let year = date.getFullYear().toString();
+			let month = (date.getMonth() + 1).toString();
+			let day = date.getDate().toString();
+			let city = localStorage.getItem('city') || '';
+			let cloud = localStorage.getItem('cloud') || '';
+			let cond = localStorage.getItem('cond') || '';
+			let dateStart = new Date(localStorage.getItem('date') || new Date());
+			let dayDiff = date.getDate() - dateStart.getDate();
+			let weatherKeys = [
+				'fbb7fed63979495f88fc1ddc7296f497', 
+				'f3488e987bce466d8ae6b523becf278f',
+				'95cf38cd40b84df9beae340c610e8550', 
+				'3cad9669ecba42c39ebfd73cdb566329', 
+				'6ec2f3eef9bc448ba8a72e815dd86f12'
+			];
+			if (city == '' || dayDiff >= 1) {
+				let weather = {};
+				for (let i = 0; i < weatherKeys.length; i++) {
+					ctx.$axios.get('https://free-api.heweather.net/s6/weather/now?location=auto_ip&key=' + weatherKeys[i]).then(res => {
+						weather = res.data.HeWeather6[0];
+						city = weather.basic.parent_city || '';
+						cloud = weather.now.fl || '';
+						cond = weather.now.cond_txt || '';
+						localStorage.setItem('city', city);
+						localStorage.setItem('cloud', cloud);
+						localStorage.setItem('cond', cond);
+						localStorage.setItem('date', new Date());
+					}).catch(err => {
+						console.log(err);
+					})
+					if (weather.status == 'ok') {
+						break;
+					}
+				}
+			}
+			citystr.value = year + '年' + month + '月' + day + '日 ' + city;
+			weatherstr.value =  cond + ' / ' + cloud + '℃';
+		}
+		const getDiary = () => {
+			ctx.$axios.get("/diary").then(res => {
+				content.value = res.data.content;
+			}).catch(err => {
+				console.log(err);
+			})
+		}
+		const addDiary = () => {
+
+		}
+
+		onMounted(() =>{
+			getweather();
+			getDiary();
+		})
+	
+		return {
+			citystr,
+			content,
+			weatherstr,
+			getDiary,
+			addDiary
+		}
+	}
+}
+</script>
--- src/components/view/words.vue
+++ src/components/view/words.vue
@@ -1,72 +1,108 @@
 <template>
-    <div>
-        <div class="content-box">
-            <el-card class="box-card">
-                <div class="content" v-cloak>
-                    <div class="text">{{ content }}</div>
-                    <div class="lickCount">👅x {{ animatedNumber }}</div>
-                </div>
-            </el-card>
-        </div>
-        <div class="buttons">
-            <el-button type="primary" @click="getWords" round>再来一条</el-button>
-            <el-button @click="lick" round :disabled="clickLick">👅</el-button>
-        </div>
-    </div>
+	<div>
+		<div class="content-box">
+			<el-card class="box-card">
+				<div class="content" v-cloak>
+					<div class="text">{{ content }}</div>
+					<div class="lickCount">👅x {{ animatedNumber }}</div>
+				</div>
+			</el-card>
+		</div>
+		<div class="buttons">
+			<el-button type="primary" @click="getWords" round>再来一条</el-button>
+			<el-button @click="lick" round :disabled="clickLick">👅</el-button>
+			<el-button type="success" @click="sendWordDialog = true" round>投稿</el-button>
+		</div>
+		<el-dialog title="投稿 舔狗の语" v-model="sendWordDialog" custom-class="send_dialog">
+			<p>你可以投稿你认为适当的内容，可以是正常的情话，当然也可以是土味情话。为了防止可能混入令人不适的内容，
+				后台会对每次提交的内容进行审核，通过方予展示，如果被判断这个句子疑似有 暴恐违禁、政治敏感、恶意推广、低俗辱骂、低质灌水 等倾向，
+				则会自动拒绝这个句子进入内容库。当然这可能造成误伤，但是为了保持内容库的清洁，还请见谅。</p>
+			<el-form :model="wordsForm">
+				<el-form-item prop="words">
+					<el-input type="textarea" :rows="9" placeholder="请输入内容" v-model="wordsForm.words"></el-input>
+				</el-form-item>
+			</el-form>
+			<template #footer>
+				<span class="dialog-footer">
+					<el-button @click="sendWordDialog = false">取 消</el-button>
+					<el-button type="primary" @click="sendWord">确 定</el-button>
+				</span>
+			</template>
+		</el-dialog>
+	</div>
 </template>
 
 <script>
-import { computed, onMounted, ref, watch, getCurrentInstance } from 'vue';
+import { computed, onMounted, ref, reactive, watch, getCurrentInstance } from 'vue';
 import { gsap } from 'gsap';
 
 export default {
-    setup(){
-        const { ctx } = getCurrentInstance();
-        const wordid = ref(0);
-        const content = ref("");
-        const lickCount = ref(0);
-        const tweenedNumber = ref(0);
-        const clickLick = ref(false);
-        const getWords = () => {
-            ctx.$axios.get("/words").then(res => {
-                wordid.value = res.data.id;
-                content.value = res.data.content;
-                lickCount.value = res.data.lickCount;
-                clickLick.value = false;
-            }).catch((err) => {
-                console.log(err);
-            });
-        };
-        const lick = () => {
-            ctx.$axios.post("/wordslick", {
-				id: wordid.value,
-			})
-			.then(function (res) {
-                clickLick.value = true;
-                lickCount.value = res.data.lickCount;
-			})
-			.catch((err) => {
+	setup(){
+		const ctx = getCurrentInstance();
+		const wordid = ref(0);
+		const content = ref("");
+		const lickCount = ref(0);
+		const tweenedNumber = ref(0);
+		const clickLick = ref(false);
+		const sendWordDialog = ref(false);
+		const wordsForm = reactive({
+			words: ""
+		})
+
+		const getWords = () => {
+			ctx.$axios.get("/words").then(res => {
+				wordid.value = res.data.id;
+				content.value = res.data.content;
+				lickCount.value = res.data.lickCount;
+				clickLick.value = false;
+			}).catch((err) => {
 				console.log(err);
 			});
-        }
-        onMounted(() => {
-            getWords();
-        })
-        const animatedNumber = computed(() => {
-            return tweenedNumber.value.toFixed(0);
-        });
-        watch(lickCount,(nv) => {
-            tweenedNumber.value = 0;
-            gsap.to(tweenedNumber, { duration: 0.5, value: nv });
-        });
-        return {
-            lickCount,
-            clickLick,
-            content,
-            getWords,
-            lick,
-            animatedNumber
-        }
-    }
+		};
+		const lick = () => {
+			ctx.$axios.post("/wordslick", {
+				id: wordid.value,
+			}).then(res => {
+				clickLick.value = true;
+				lickCount.value = res.data.lickCount;
+			}).catch(err => {
+				console.log(err);
+			});
+		}
+		const sendWord = () => {
+			ctx.$axios.post("/addWords",wordsForm).then(() => {
+				ctx.$message({
+					message: '恭喜你，提交成功，审核后即可显示',
+					type: 'success'
+				});
+				sendWordDialog.value = false;
+				wordsForm.words = "";
+			}).catch((err) => {
+				console.log(err);
+			});
+		}
+
+		onMounted(() => {
+			getWords();
+		})
+		const animatedNumber = computed(() => {
+			return tweenedNumber.value.toFixed(0);
+		});
+		watch(lickCount,(nv) => {
+			tweenedNumber.value = 0;
+			gsap.to(tweenedNumber, { duration: 0.5, value: nv });
+		});
+		return {
+			lickCount,
+			clickLick,
+			content,
+			getWords,
+			lick,
+			sendWordDialog,
+			wordsForm,
+			sendWord,
+			animatedNumber
+		}
+	}
 }
 </script>
